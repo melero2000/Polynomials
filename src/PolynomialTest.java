03ca14f857ec8263bf834455fb0c889620681ccf
alu54281131g <alu54281131g@ti2-08>
2018-11-13 19:58:36 +0100

    Finished

--- src/PolynomialTest.java
+++ src/PolynomialTest.java
@@ -3,7 +3,7 @@ public class PolynomialTest {
 	public static void main(String[] args) {
 		MyPolynomial p1 = new MyPolynomial(2, 2, 1, 2);
 		MyPolynomial p2 = new MyPolynomial(1, 1, 2);
-	
+ 	
 		System.out.println(p1.multiply(p2));
 	}
 }
